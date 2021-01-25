cf1e6dc0e1186c166a95d4b9dfa14c4748858835
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2021-01-25 22:02:46 +0000

    made editable

--- snippets/src/index.js
+++ snippets/src/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -18,6 +19,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
 };
+
 
 app.on('ready', createWindow);
 

abd483a7927fb9fa87303c115c8c75d946066fe2
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2021-01-27 21:39:08 +0000

    added readFile for the .txt files

--- PSL/src/loader.js
+++ PSL/src/loader.js
@@ -6,7 +6,13 @@ const path = app.getPath("appData") +"/psl/snippets"
 fs.readdir(path, (err, files) => {
     if (err) return console.log(err);
     for (let file of files){
+        fs.readFile(path + "/" + file, (err, data) => {
+            if (err) return console.log(err);
+            console.log(data.toString());
+          });
         let fileName = file.replace(".txt", "");
         console.log(fileName);  
     }
   });
+
+

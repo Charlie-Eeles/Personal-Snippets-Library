bd256ca4fb151e2b7d455606a5073aab21e6d4dc
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2021-01-27 22:35:15 +0000

    2 changes
    mopved render.js code to loader.js because they overlapped and now the monaco editor is filled out with the contents of the first .txt

--- PSL/src/loader.js
+++ PSL/src/loader.js
@@ -1,18 +1,46 @@
 const fs = require("fs");
 const { app } = require("electron").remote;
+const load = require("monaco-loader");
 
 const path = app.getPath("appData") +"/psl/snippets"
+const monacoDiv = document.getElementById("editor")
+
+load().then((monaco) => {
+    
+    codeEditor = monaco.editor.create(monacoDiv, {
+        language: "javascript",
+        theme: "vs-dark",
+        automaticLayout: true
+    })
+})
+
 
 fs.readdir(path, (err, files) => {
     if (err) return console.log(err);
+    let fileArray = [];
     for (let file of files){
+        let innerArray = [];
         fs.readFile(path + "/" + file, (err, data) => {
             if (err) return console.log(err);
-            console.log(data.toString());
+            innerArray.push(data.toString());
           });
         let fileName = file.replace(".txt", "");
-        console.log(fileName);  
+        innerArray.push(fileName)
+        fileArray.push(innerArray)
     }
+
+    let list = document.querySelector(".snippet-list");
+
+    for(let i=1; i<fileArray.length;i++){
+        let item = document.createElement("li");
+        item.appendChild(document.createTextNode(fileArray[i][0]));
+        list.appendChild(item);
+    }
+
+    let title = document.querySelector(".title");
+    title.innerHTML = fileArray[0][1];
+    load().then(() => {codeEditor.setValue(fileArray[0][1]);})
+    console.log(fileArray[0])
   });
 
 
